35ee3f94597d8312db3b982e20e55de6604ff8e2
KevinFdz <kevinfdz98@gmail.com>
2020-05-01 15:57:17 -0500

    Added database and methods using it

---
+++ models/bookmarksModel.js
@@ -0,0 +1,122 @@
+const mongoose = require('mongoose'); 
+
+const bookmarksSchema = mongoose.Schema({
+    id : {
+        type : String, 
+        unique : true
+
+    },
+
+    title : {
+        type : String
+    },
+
+    description : {
+        type : String
+
+    },
+    url : {
+        type : String
+
+    }, 
+    rating : {
+        type : Number
+    }
+
+}); 
+
+const bookmarksCollection = mongoose.model('bookmarksDB', bookmarksSchema); 
+
+const bookmarksQueries = {
+    createBookmark : function( newBookmark ){
+        return bookmarksCollection
+                .create( newBookmark )
+                .then( createdBookmark => {
+                        return createdBookmark; 
+                })
+                .catch( err => {
+                    return err; 
+                }) 
+    },
+
+    getBookmarks : function(){
+        return bookmarksCollection
+                .find()
+                .then( gettingCollection => {
+                        return gettingCollection; 
+                })
+                .catch(err => {
+                    return err; 
+                })
+    }, 
+
+    getBookmarksByTitle : function(searchTitle){
+        return bookmarksCollection
+        .find({'title' : searchTitle })
+        .then( gettingFiles => {
+                    return gettingFiles
+        })
+        .catch(err => {
+            return err; 
+        })
+    }, 
+
+    
+    getBookmarkById : function(idToFind){
+        return bookmarksCollection
+        .find({'id' : idToFind })
+        .then( gettingFiles => {
+                    return gettingFiles
+        })
+        .catch(err => {
+            return err; 
+        })
+    },
+
+    removeElementById : function(idToRemove){
+        return bookmarksCollection
+        .remove({'id' : idToRemove})
+        .then( removed => {
+                return removed
+        })
+        .catch(err => {
+            return err; 
+        })
+    },
+
+   /* patchElementById : function(idToPatch, objectsToUpdate){
+        return bookmarksCollection
+        .findByIdAndUpdate({'id' : idToPatch}, { update : {objectsToUpdate}})
+        .then( updated => {
+                return updated
+        })
+        .catch(err => {
+            return err; 
+        })
+    }*/
+
+   /* patchElementById : function(idToPatch, objectsToUpdate){
+        return bookmarksCollection
+        .update({'id' : idToPatch}, { $set : {objectsToUpdate}})
+        .then( updated => {
+                return updated
+        })
+        .catch(err => {
+            return err; 
+        })
+    }*/
+
+    patchElementById : function(idToPatch, objectsToUpdate){
+        return bookmarksCollection
+        .update({'id' : idToPatch},{objectsToUpdate})
+        .then( updated => {
+                return updated
+        })
+        .catch(err => {
+            return err; 
+        })
+    }
+}
+
+
+module.exports = { bookmarksQueries }; 
